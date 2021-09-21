59b5be4ad3fe5547cac24575bc6df7d3827a6d35
Quicknicker <50339024+Quicknicker@users.noreply.github.com>
2021-09-21 12:42:32 +0200

    Updated beta-file
    new features:
    - better logging
    - playlist downloads (dir for items is created; shown in log)
    - long videos (>1H) get marked in log
    - debugging
    - verbose log (WIP)

--- ytdl-dM_beta.ps1
+++ ytdl-dM_beta.ps1
@@ -1,7 +1,6 @@
-#The beta-version is outdated. Using main file is recommended.
 #Caution! This file is more vulnerable to errors than the stable version.
- 
-#v27122020
+
+#v21092021
 #NB
 
 $ytdl_dir = ""                                          # write YouTube-dl location here
@@ -10,14 +9,115 @@ $ffmpeg_location = "$ytdl_dir\ffmpeg\bin"               # change ffmpeg location
 $dir = ""                                               # write target location here
 $logdir = "$dir\download-log.txt"                       # change log location if needed
 $isLogging = $true                                      # toggle logging
-
+$vLogdir = "$dir\verboseLog.txt"
+$verboseLog = $false                                    # toggle verbose logging
+$ytAPIKey = ""                                          # write your YouTubeAPI-Key here
 
 $url = ""
+$errorCount = 0
 
 
 # functions
 
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+
+
+function Get-VideoInfo {
+    param(
+        [Parameter(Mandatory = $True)]
+        [string]$videoUrl,
+
+        [Parameter(Mandatory = $True)]
+        [ValidateSet('title', 'duration', 'id', 'etag', 'playlistId', 'itemLength', 'pTitle')]
+        [string]$infoType,
+
+        [Parameter(Mandatory = $False)]
+        [boolean]$isPlaylist = $False
+    )
+
+    if ($isPlaylist -eq $False)
+    {
+
+        $regex = '[^ ]*v='
+        $videoID = $videoURL -replace $regex
+        $regex = '&[^ ]*'
+        $videoID = $videoID -replace $regex
+        $metadata = irm "https://www.googleapis.com/youtube/v3/videos?id=$videoID&key=$ytAPIKey&part=snippet&part=contentDetails"
+        if ($infoType -eq 'duration')
+        {
+            $videoDuration = $metadata.items.contentDetails.duration
+
+            $regex = '(^[A-Z]*)'
+            $videoDuration = $videoDuration -replace $regex
+
+            $regex = 'H[^ ]*'
+            $videoM = $videoDuration -replace $regex
+            [int]$videoM *= 60
+
+            $regex = 'M[^ ]*'
+            $videoM2 = $videoDuration -replace $regex
+            $regex = '[^ ]*H'
+            $videoM += $videoM2 -replace $regex
+
+            $regex = '[^ ]*M'
+            $videoS = $videoDuration -replace $regex
+            $regex = 'S[^ ]*'
+            $videoS = $videoS -replace $regex
+
+            $videoInfo = $videoM*60 + $videoS
+
+        } elseif ($infoType -eq 'title')
+        {
+            $videoInfo = $metadata.items.snippet.title
+        } elseif ($infoType -eq 'id')
+        {
+            $videoInfo = $metadata.items.id
+        } elseif ($infoType -eq 'etag')
+        {
+            $videoInfo = $metadata.etag
+        }
+
+    } else
+    {
+        $regex = '[^ ]*list='
+        $listID = $videoURL -replace $regex
+        $regex = '&[^ ]*'
+        $listID = $listID -replace $regex
+        $metadata = irm "https://www.googleapis.com/youtube/v3/playlistItems?playlistId=$listID&key=$ytAPIKey&maxResults=50&part=snippet&part=contentDetails"
+
+        $videoInfo = @()
+
+        if ($infoType -eq 'title')
+        {
+            foreach ($item in $metadata.items)
+            {
+                $videoInfo += $item.snippet.title
+            }
+        } elseif ($infoType -eq 'id')
+        {
+            foreach ($item in $metadata.items)
+            {
+                $videoInfo += $item.contentDetails.videoId
+            }
+        } elseif ($infoType -eq 'etag')
+        {
+            $videoInfo = $metadata.etag
+        } elseif ($infoType -eq 'playlistId')
+        {
+            $videoInfo = $listID
+        } elseif ($infoType -eq 'itemLength')
+        {
+            $videoInfo = $metadata.pageInfo.totalResults
+        } elseif ($infoType -eq 'pTitle')
+        {
+            $metadata = irm "https://www.googleapis.com/youtube/v3/playlists?id=$listID&key=$ytAPIKey&part=snippet&part=contentDetails"
+            $videoInfo = $metadata.items[0].snippet.title
+        }
+
+    }
+    
+    $videoInfo
+}
 
 function Set-WindowState {
 	<#
@@ -84,6 +184,37 @@ public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 	}
 }
 
+function verboseLog {
+        # create log at first launch
+        if (-not [System.IO.File]::Exists($vLogdir))
+        {
+            New-Item -Path "$vLogdir" -ItemType File
+        }
+
+
+        # get info
+        if ($vBoth -eq 2 -or $sub -eq 'y' -or $quality -eq 'b')
+        {
+            $title = [System.IO.Path]::GetFileNameWithoutExtension("$dir\$((Get-ChildItem $dir) | Sort-Object CreationTime -Descending | Select-Object -First 1).Name")
+        } else
+        {
+            $title = Get-VideoInfo -videoUrl $url -infoType title
+            $videoID = Get-VideoInfo -videoUrl $url -infoType id
+            $videoEtag = Get-VideoInfo -videoUrl $url -infoType etag
+            if ($type -eq 'V' -or $type -eq 'Video')
+            {
+                $title = $title + '.mp4'
+            } elseif ((($type -eq 'Audio') -or ($type -eq 'A')) -and ($quality -eq 'mp3' -or $quality -eq 'm'))
+            {
+                $title = $title + '.mp3'
+            } elseif ((($type -eq 'Audio') -or ($type -eq 'A')) -and ($quality -eq 'opus' -or $quality -eq 'o'))
+            {
+                $title = $title + '.opus'
+            }
+        }
+        Add-Content $vLogdir -Value "$(Get-Date -Format "MM/dd/yyyy HH:mm")      $title              Id= $videoID; Etag= $videoEtag"
+}
+
 Add-Type @"
   using System;
   using System.Runtime.InteropServices;
@@ -98,7 +229,7 @@ Add-Type @"
 # URL
 $Geturl = {
     $promptURL = Read-Host -Prompt 'Enter URL or type "C"'
-    if ($promptURL -eq 'C')
+    if (($promptURL -eq 'C') -or $promptURL -eq '')
     {
         if ((Get-Clipboard) -eq $null)
         {
@@ -124,13 +255,17 @@ $Geturl = {
             .$Geturl
         }
     }
+
+    # delete timecode
+    $regex = '&t[^ ]*'
+    $url = $url -replace $regex
 }
 
 # process
 $download = {
     cls
 
-
+    # set params
 
     .$Geturl
 
@@ -141,7 +276,7 @@ $download = {
 
         # subtitles / vQuality
         $vBoth = 1
-        if (($type -eq 'Video') -or ($type -eq 'V'))
+        if (($type -eq 'Video') -or ($type -eq 'V') -or ($type -eq ''))
         {
             Write-Host 'Y / N'
             $sub = Read-Host -Prompt 'Subtitle?'
@@ -186,7 +321,7 @@ $download = {
     # download
     #$ErrorActionPreference = "SilentlyContinue"
 
-    if ((($type -eq 'Video') -or ($type -eq 'V')) -and (($sub -eq 'n') -or ($sub -eq '')))
+    if ((($type -eq 'Video') -or ($type -eq 'V') -or ($type -eq '')) -and (($sub -eq 'n') -or ($sub -eq '')))
     {
 
         if ($vBoth -eq 2)
@@ -209,9 +344,12 @@ $download = {
         }
         if ($vBoth -eq 2)
         {
-            $cmd_process = (Get-Process youtube-dl).Id
+
+            $ErrorActionPreference = 'Ignore'
+            $cmd_process = (Get-Process youtube-dl -ErrorAction Ignore).Id
             Wait-Process $cmd_process
             Start-Sleep -Seconds 1
+            $ErrorActionPreference = 'Continue'
         
             $title1 = ((Get-ChildItem $dir) | Sort-Object CreationTime -Descending | Select-Object -Skip 1 | Select-Object -First 1).Name
             $title2 = ((Get-ChildItem $dir) | Sort-Object CreationTime -Descending | Select-Object -First 1).Name
@@ -234,9 +372,11 @@ $download = {
         
         Start-Process $youtube_dl_location -ArgumentList ("-o $dir\%(title)s.%(ext)s -f $fquality --ignore-config --hls-prefer-native --write-sub --ffmpeg-location $ffmpeg_location --merge-output-format mp4 $url")
         
-        $cmd_process = (Get-Process youtube-dl).Id
+        $ErrorActionPreference = 'Ignore'
+        $cmd_process = (Get-Process youtube-dl -ErrorAction Ignore).Id
         Wait-Process $cmd_process
         Start-Sleep -Seconds 1
+        $ErrorActionPreference = 'Continue'
         
         $title1 = ((Get-ChildItem $dir) | Sort-Object CreationTime -Descending | Select-Object -Skip 1 | Select-Object -First 1).Name
         $title2 = ((Get-ChildItem $dir) | Sort-Object CreationTime -Descending | Select-Object -First 1).Name
@@ -255,7 +395,7 @@ $download = {
         {
             Start-Process $youtube_dl_location -ArgumentList ("-o $dir\%(title)s.%(ext)s -x --audio-format best --audio-quality 0 --ignore-config --hls-prefer-native --ffmpeg-location $ffmpeg_location $url")
         }
-        elseif (($quality -eq 'mp3') -or ($quality -eq 'm'))
+        elseif (($quality -eq 'mp3') -or ($quality -eq 'm') -or ($quality -eq ''))
         {
             Start-Process $youtube_dl_location -ArgumentList ("-o $dir\%(title)s.%(ext)s -x --audio-format mp3 --audio-quality 0 --ignore-config --hls-prefer-native --ffmpeg-location $ffmpeg_location $url")
         }
@@ -281,11 +421,13 @@ $download = {
         else
         {
             Write-Host 'Thats not a valid quality'
+            $errorCount += 1
         }
     }
     else
     {
         Write-Host 'Thats not a valid filetype'
+        $errorCount += 1
     }
 
     Write-Host ''
@@ -301,32 +443,117 @@ $download = {
     $ErrorActionPreference = 'Continue'
 
     # append log
-    if (($isLogging -eq $true) -and ($Error.Count -eq 0))
+    if (($isLogging -eq $true) <#-and ($Error.Count -eq 0)#> -and ($errorCount -eq 0))
     {
+        
+
+        # create log at first launch
+        if (-not [System.IO.File]::Exists($logdir))
+        {
+            New-Item -Path "$logdir" -ItemType File
+        }
+
+        # log playlists
+        if ($url.Contains('playlist?list='))
+        {
+            $title = Get-VideoInfo -videoUrl $url -infoType title -isPlaylist $True
+            $videoID = Get-VideoInfo -videoUrl $url -infoType id -isPlaylist $True
+            $videoEtag = Get-VideoInfo -videoUrl $url -infoType etag -isPlaylist $True
+            if ($type -eq 'V' -or $type -eq 'Video')
+            {
+                $title = $title + '.mp4'
+            } elseif ((($type -eq 'Audio') -or ($type -eq 'A')) -and ($quality -eq 'mp3' -or $quality -eq 'm'))
+            {
+                $title = $title + '.mp3'
+            } elseif ((($type -eq 'Audio') -or ($type -eq 'A')) -and ($quality -eq 'opus' -or $quality -eq 'o'))
+            {
+                $title = $title + '.opus'
+            }
+            
+            Add-Content $logdir -Value "$(Get-Date -Format "MM/dd/yyyy HH:mm")      PlaylistDownload      Id= $(Get-VideoInfo -videoUrl $url -infoType playlistId -isPlaylist $True); Etag= $videoEtag"
+            for ($i = 0; $i -lt (Get-VideoInfo -videoUrl $url -infoType itemLength -isPlaylist $True); $i++)
+            {
+                Add-Content $logdir -Value "       " -NoNewline; Add-Content $logdir -Value $title[$i] -NoNewline; Add-Content $logdir -Value "      Id= " -NoNewline; Add-Content $logdir -Value $videoID[$i]
+            }
+
+        } else
+        {
+
+            # append new file to log
+            # if vid is longer than 1H -> addon
+            if ((Get-VideoInfo -videoUrl $url -infoType duration) -gt 3600)
+            {
+                $addon = '      TIME CAN DIFFER'
+            }
+
+            # get title of video
+            if ($vBoth -eq 2 -or $sub -eq 'y' -or $quality -eq 'bo' -or $quality -eq 'both')
+            {
+                $title = [System.IO.Path]::GetFileNameWithoutExtension("$dir\$((Get-ChildItem $dir) | Sort-Object CreationTime -Descending | Select-Object -First 1).Name")
+            } else
+            {
+                $title = Get-VideoInfo -videoUrl $url -infoType title
+                $videoID = Get-VideoInfo -videoUrl $url -infoType id
+                $videoEtag = Get-VideoInfo -videoUrl $url -infoType etag
+                if ($type -eq 'V' -or $type -eq 'Video')
+                {
+                    $title = $title + '.mp4'
+                } elseif ((($type -eq 'Audio') -or ($type -eq 'A')) -and ($quality -eq 'mp3' -or $quality -eq 'm'))
+                {
+                    $title = $title + '.mp3'
+                } elseif ((($type -eq 'Audio') -or ($type -eq 'A')) -and ($quality -eq 'opus' -or $quality -eq 'o'))
+                {
+                    $title = $title + '.opus'
+                }
+            }
+            Add-Content $logdir -Value "$(Get-Date -Format "MM/dd/yyyy HH:mm")      $title$addon              Id= $videoID; Etag= $videoEtag"
+        }
+    }
+
+    if (($url.Contains('playlist?list=')) -and ($errorCount -eq 0))
+    {
+        Start-Sleep -Seconds 1
+        $cmd_process = (Get-Process youtube-dl).Id
+        Wait-Process $cmd_process
+        Start-Sleep -Seconds 3
+
+        $titles = @();
+        for ($i = 0; $i -lt (Get-VideoInfo -videoUrl $url -infoType itemLength -isPlaylist $True); $i++)
+        {
+            $titles += ((Get-ChildItem $dir) | Sort-Object CreationTime -Descending | Select-Object -Skip $i | Select-Object -First 1).Name
+        }
+
+        $pTitle = (Get-VideoInfo -videoUrl $url -infoType pTitle -isPlaylist $True)
+        New-Item -Path "$dir\$pTitle" -ItemType Directory | Out-Null
+
+        <#for ($i = 0; $i -lt (Get-VideoInfo -videoUrl $url -infoType itemLength -isPlaylist $True); $i++)
+        {
+            Move-Item $dir\$titles[$i] $dir\$pTitle
+        }#>
+
+        foreach ($title in $titles)
+        {
+            Move-Item $dir\$title $dir\$pTitle
+        }
+    }
+
+    if ($verboseLog -eq $true)
+    {
+        verboseLog
+    }
+        
+    # add another URL
+    if ($vBoth -eq 2 -or $sub -eq 'y' -or $quality -eq 'b')
+    {
+        # wait until download finished
         $cmd_process = (Get-Process youtube-dl -erroraction 'silentlycontinue').id
 
         if ($cmd_process -ne $null -and $cmd_process -ne 0)
         {
             Wait-Process $cmd_process
         }
-
-
-        #$pwsh_process | Set-WindowState -State SHOW
-        #Write-Host $Error.Count
-        #Write-Host $output
-        #if ($output -match 'ERROR')
-        #{
-        #    Write-Host "testtest"
-        #}
-        #Read-Host "Press enter to continue..."
-        
-
-
-        $title = [System.IO.Path]::GetFileNameWithoutExtension("$dir\$((Get-ChildItem $dir) | Sort-Object CreationTime -Descending | Select-Object -First 1).Name")
-        Add-Content $logdir -Value "$(Get-Date -Format "MM/dd/yyyy HH:mm")      $title"
     }
-        
-    # add another URL
+
     $result = "Proceed", "Add URL" | Out-GridView -PassThru -Title "Choose option"
         
     if ($result -eq 'Add URL')
@@ -335,6 +562,7 @@ $download = {
         &$download
     }
     
+    # hide toggle
     $ytdl_process | Set-WindowState -State SHOW
         
     $cmd_process = (Get-Process youtube-dl -erroraction 'silentlycontinue').id
@@ -344,7 +572,6 @@ $download = {
     Wait-Process $cmd_process
     }
 
-    # hide toggle
     $pwsh_process | Set-WindowState -State SHOW
 
     # finish
